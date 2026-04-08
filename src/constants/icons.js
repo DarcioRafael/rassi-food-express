0718191a2036a6b26916770ebb75435a6a005c36
DarcioRafael <liminhadarcioo@gmail.com>
2026-04-08 20:43:56 -0300

    ultimo comit: 9c8b1e2 - Atualização do perfil e adição de ícones (2024-06-15)

--- src/constants/icons.js
+++ src/constants/icons.js
@@ -8,6 +8,10 @@ import favoritofull from "../assets/favorito-full2.png";
 import favorito from "../assets/favorito.png";
 import logo01 from "../assets/logo01.png";
 import logo02 from "../assets/logo02.png";
+import dados from "../assets/dados.png";
+import endereco from "../assets/endereco.png";
+import expandir from "../assets/expandir.png";
+import logout from "../assets/logout.png";
 
 
-export default { logo01, logo02, abahome, abafavorito, abapedido, abaperfil, cart, favorito, favoritofull, };
+export default { logo01, logo02, abahome, abafavorito, abapedido, abaperfil, cart, favorito, favoritofull, dados, endereco, expandir, logout };
